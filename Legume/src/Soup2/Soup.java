b581c813be654305c524a6fc02b98bbf87ca679f
Nicolas Berat <nicolas.berat@epita.fr>
2018-09-06 09:33:01 +0200

    add work to the git

---
+++ Legume/src/Soup2/Soup.java
@@ -0,0 +1,40 @@
+package Soup2;
+
+import java.util.ArrayList;
+
+public class Soup
+{
+  ArrayList<Vegetable> legumes;
+
+  public Soup()
+  {
+    legumes = new ArrayList<Vegetable>();
+  }
+
+  public void add(Vegetable v)
+  {
+    legumes.add(v);
+  }
+
+  public double getPeelingWeight()
+  {
+    double r = 0;
+    for (Vegetable legume : legumes)
+    {
+      r += legume.poids * legume.peelingWeight;
+      if (legume instanceof Potatoe)
+        r += 0.01 * legume.car;
+    }
+    return r;
+  }
+
+  public String toString()
+  {
+    String s = "";
+    for (Vegetable legume : legumes)
+    {
+      s += legume.toString() + System.lineSeparator();
+    }
+    return s;
+  }
+}
