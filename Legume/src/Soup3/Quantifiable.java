2d770e29095611874870dbe26b74af067b5696a8
Nicolas Berat <nicolas.berat@epita.fr>
2018-09-06 11:45:09 +0200

    begin implemented quantifiable

---
+++ Legume/src/Soup3/Quantifiable.java
@@ -0,0 +1,24 @@
+package Soup3;
+
+public class Quantifiable {
+	public Soup soup;
+	public Carrot carrot;
+	public Potatoe potatoe;
+	public Quantifiable() {
+		soup = s;
+		carrot = c;
+		potatoe = p;
+	}
+	
+	public String stringQuantifiable(Soup s) {
+		return s.toString();
+	}
+	
+	public String stringQuantifiable(Carrot c) {
+		return c.toString();
+	}
+	
+	public String stringQuantifiable(Potatoe p) {
+		return p.toString();
+	}
+}
--- Legume/src/Soup3/Test.java
+++ Legume/src/Soup3/Test.java
@@ -5,12 +5,11 @@ public class Test
   public static void main(String[] args)
   {
     Potatoe p1 = new Potatoe(0.3, 10);
-    System.out.println(p1); // affiche: 86
     Carrot c1 = new Carrot(0.25, 30);
-    System.out.println(c1); // affiche: 90
     Soup s = new Soup();
     s.add(p1);
     s.add(c1);
-    System.out.println(s); // affiche: 176
+    System.out.println("Les quantifiables :");
+    Quantifiable quantifiables[] = {p1, c1, s};
   }
 }
